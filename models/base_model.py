319ce16c4da81f006c76b6b6b87474f1e35f4688
ZuhairAhmed0 <zuh12390@gmail.com>
2023-03-12 04:07:48 -0700

    last update

--- models/base_model.py
+++ models/base_model.py
@@ -7,20 +7,35 @@ class BaseModel():
     """ BaseModel that defines all common attributes/
     methods for other classes """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *arg, **kwargs):
+        """Public instance artributes initialization
+        Args:
+            *args(args): arguments
+            **kwargs(dict): attrubute values
+        """
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+        else:
+            for key, value in kwargs.item():
+                if key in ("created_at", "updated_at"):
+                    self.__dict__[key] = datetime.now()
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
         dict_copy = self.__dict__.copy()
-        dict_copy["__class__"] = self.__class__.__name__
         dict_copy["created_at"] = self.created_at.isoformat()
         dict_copy["updated_at"] = self.updated_at.isoformat()
+        dict_copy["__class__"] = self.__class__.__name__
         return dict_copy
